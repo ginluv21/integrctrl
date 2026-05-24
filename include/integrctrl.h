79a83d102b47d7c22d341fe7df059532b8f64a13
Gabik <gingers1over.g@gmail.com>
2026-05-24 17:43:26 +0700

    добавить структуры prog_opts, file_rec и разбор аргументов

--- include/integrctrl.h
+++ include/integrctrl.h
@@ -1,27 +1,34 @@
 #ifndef INTEGRCTRL_H
 #define INTEGRCTRL_H
 
+#include "md5.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h> // для ssize_t
-#include "md5.h"
 
 #define NAME_LEN 256
 
 #define NAME_LEN 256
 
-enum {
-    TYPE_DIR,
-    TYPE_FILE
-};
+enum { TYPE_DIR, TYPE_FILE };
 
 typedef struct file_rec {
-    int id;
-    char name[NAME_LEN];
-    int type;
-    int parent_id;
-    unsigned char md5[16];
-};
+    int id; // Уникальный идентификатор записи
+    char name[NAME_LEN]; // Имя файла или директории
+    int type; // Тип: TYPE_DIR (директория) или TYPE_FILE (файл)
+    int parent_id; // Идентификатор родительской директории (0 для корневой)
+    unsigned char md5[16]; // MD5-хеш для файлов
+}file_rec;
+
+typedef struct prog_opts {
+    int mode; // Режим работы: s (save) или c (check)
+    int recursive; // Флаг рекурсии: 1 (включена) или 0 (выключена)
+    char *db_file; // Строка с именем файла базы данных
+    char *target_dir; // Строка с именем анализируемой папки
+}prog_opts;
+
+void parse_args(int argc, char *argv[], prog_opts *opts);
+void print_usage(const char *prog_name);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,21 @@
 #include "integrctrl.h"
 
 int main(int argc, char *argv[]) {
+    prog_opts opts;
+
+    parse_args(argc, argv, &opts);
+
+    printf("===Проверка разбора через структуру===\n");
+    printf("Режим работы: %c\n", opts.mode);
+    printf("Рекурсия: %d\n", opts.recursive);
+    printf("Файл базы данных: %s\n", opts.db_file);
+    printf("Папка для анализа: %s\n", opts.target_dir);
+
+    if(opts.mode == 's'){
+        printf("\n[Сохранение] Начинаем обход директории %s...\n", opts.target_dir);
+    } else {
+        printf("\n[Проверка] Начинаем сверку с базой %s...\n", opts.db_file);
+    }
+
     return 0;
 }
